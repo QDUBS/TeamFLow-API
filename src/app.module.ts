1e4e579feef822a2511092cd9c09a4832b80905b
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-13 23:27:43 +0100

    Update env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,8 +15,8 @@ import { UserModule } from './user/user.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('HOST'),
-        port: configService.get('PORT'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
