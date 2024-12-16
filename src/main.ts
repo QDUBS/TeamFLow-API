d948264bdb1a5fd3d9f313564e464fae12a6bd98
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-16 10:20:06 +0100

    Fix cors error IV

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(csurf());
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
+  app.use(cookieParser());
+  app.use(csurf({ cookie: { sameSite: true } }));
+
+  app.use((req: any, res: any, next: any) => {
+    const token = req.csrfToken();
+    res.cookie('XSRF-TOKEN', token);
+    res.locals.csrfToken = token;
+
+    next();
   });
   await app.listen(process.env.PORT ?? 5000);
 }
