5d3ef41f41561d59e3ac1e805807ff3fcd230c6e
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-16 10:06:29 +0100

    Fix cors error II

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(csurf());
   app.enableCors({
     origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
