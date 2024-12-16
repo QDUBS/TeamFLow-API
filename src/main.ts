4722c109876cd8ddc43c663256621d1dce6970d0
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-16 12:54:02 +0100

    Fix cors error VI

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,13 @@ import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(csurf());
-  // app.enableCors({
-  //   origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  // });
   app.use(
     session({
       secret: `${process.env.JWT}`,
