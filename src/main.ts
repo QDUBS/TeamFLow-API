351d8994b5867964ee619456285764d3bb600a58
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-16 13:01:33 +0100

    Fix cors error VII

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,35 @@ import * as csurf from 'csurf';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(csurf());
-  app.use(
-    session({
-      secret: `${process.env.JWT}`,
-      resave: false,
-      saveUninitialized: false,
-      cookie: { secure: true }, // Set to true if using HTTPS
-    }),
-  );
-  app.use(csurf());
-  await app.listen(process.env.PORT ?? 5000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    app.enableCors({
+      origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+
+    const port = process.env.PORT || 5000;
+    await app.listen(port, '0.0.0.0');
+    console.log(
+      `Application running on port ${port} ${process.env.SUPABASE_URL}`,
+    );
+  } catch (error) {
+    console.error('Application startup error:', error);
+  }
 }
 bootstrap();
