b97f2e54a9e2b91e39c612d5c32bf9196d2df3de
“QDUBS” <QDUBS@users.noreply.github.com>
2024-12-16 13:51:00 +0100

    Fix cors error XI

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as dotenv from 'dotenv';
-import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
 
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
-
-    // app.enableCors({
-    //   origin: ['http://localhost:3000', 'https://team-flow-one.vercel.app'],
-    //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    //   allowedHeaders: ['Content-Type', 'Authorization'],
-    // });
-
-    // const port = process.env.PORT || 5000;
-    // await app.listen(port, '0.0.0.0');
-    // console.log(
-    //   `Application running on port ${port}`,
-    // );
-    app.use(cors());
-  } catch (error) {
-    console.error('Application startup error:', error);
-  }
+  app.enableCors(options);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
